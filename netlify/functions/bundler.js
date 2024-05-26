2710f9a03d3917181a0268abf16a46a54e3ede9b
Cornelius Jideofor <cornelius.jideofor@transferwise.com>
2024-05-26 13:13:23 +0100

    translation bundle request test

--- netlify/functions/bundler.js
+++ netlify/functions/bundler.js
@@ -1,24 +1,24 @@
 
 
-exports.handler = async data => {
+// exports.handler = async data => {
 
-    const body = data.req;
+//     const body = data.req;
 
-    const result = body.strings.map(s => {
-        return {
-          id: s.identifier,
-          text: s.text,
-          translation: s.translations[data.languageId].text
-        }
-    })
+//     const result = body.strings.map(s => {
+//         return {
+//           id: s.identifier,
+//           text: s.text,
+//           translation: s.translations[data.languageId].text
+//         }
+//     })
     
-    return JSON.stringify(result);
-}
+//     return JSON.stringify(result);
+// }
 
 exports.handler = async (req, res) => {
     const { body } = req;
     return res.send(JSON.stringify(body));
-    
+
 //   const result = body.strings.map(s => {
 //     return {
 //       id: s.identifier,
